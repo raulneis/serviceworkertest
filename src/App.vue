dd72e248e385eb79daf4cbbc6b6af5edcfae2e69
Raul Neis <raulneis@gmail.com>
2018-04-11 19:22:19 -0300

    f7

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,68 @@
 <template>
-  <div>
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error delectus totam ipsam doloremque, et. Alias recusandae illo itaque quo labore, perferendis nulla vero sunt vel, laudantium, similique esse ducimus atque.
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      fixed
+      v-model="drawer"
+      app
+    >
+      <v-list dense>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>home</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Home</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile @click="">
+          <v-list-tile-action>
+            <v-icon>contact_mail</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>Contact</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-toolbar>
+    <v-content>
+      <v-container fluid fill-height>
+        <v-layout
+          justify-center
+          align-center
+        >
+          <v-flex text-xs-center>
+            <v-tooltip left>
+              <v-btn icon large href="#" target="_blank" slot="activator">
+                <v-icon large>code</v-icon>
+              </v-btn>
+              <span>Source</span>
+            </v-tooltip>
+            <v-tooltip right>
+              <v-btn icon large href="https://codepen.io/johnjleider/pen/rJdVMq" target="_blank" slot="activator">
+                <v-icon large>mdi-codepen</v-icon>
+              </v-btn>
+              <span>Codepen</span>
+            </v-tooltip>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-content>
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2017</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+ data: () => ({
+    drawer: null
+  })
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import App from './App'
+import Vuetify from 'vuetify'
 import router from './router'
+ 
+Vue.config.productionTip = false
+Vue.use(Vuetify)
+import 'vuetify/dist/vuetify.min.css'
+
 import './assets/styles/app.scss'
 import './assets/scripts/app.js'
-
-Vue.config.productionTip = false
 
 new Vue({
   el: '#app',
